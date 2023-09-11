b38b72816f950704b4b02ce5440aa4491ed64f81
Lucja Roczniak <l.roczniak@asdag.com>
2023-09-11 21:44:13 +0200

    add font, add routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Logo from "./components/common/Logo";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Logo/>,
+    },
+    {
+        path: "/ksiegowi",
+        element: <App/>,
+    },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
